5b45c28f4f02d4872b504cd3d0a3958d505e6c8b
sameerjagdale <sameer.jagdale@mail.mcgill.ca>
2015-07-05 22:08:48 -0400

    Removed kiosk mode settings
    And also added captions to images. Added an array of subreddits from which one is chosen randomly.

--- app.js
+++ app.js
@@ -7,16 +7,15 @@ function getURLs(sr, callback) {
         children.forEach(function(val) {
             if(val.kind !== 't3') 
                 return;
-            
             var url = val.data.url;
+            var title = val.data.title;
             if(isImage(url)) {
-                links.push(url);
+                links.push({'url':url,'title':title});
             } else {
                 if(/imgur.com/.test(val.data.domain)) {
                     resourceCount++;
-                    getImgurImages(url, links, function() {
+                    getImgurImages(url, links, title, function() {
                         resourceCount--;
-
                         if(resourceCount === 0) {
                             callback(links);
                         }
@@ -31,7 +30,7 @@ function isImage(url) {
     return /\.(gif|jpg|png)$/.test(url);
 }
 
-function getImgurImages(url, links, callback) {
+function getImgurImages(url, links, title, callback) {
     $.get(url, function(res){
         //console.log(res);
         var m,
@@ -39,7 +38,7 @@ function getImgurImages(url, links, callback) {
         
         while ( m = rex.exec( res ) ) {
             if(isImage(m[1])) {
-                links.push("https://" + m[1]);
+                links.push({'url':"https://" + m[1] , 'title':title});
             }
         }
         
@@ -59,7 +58,7 @@ function processLinks(links) {
             if(e.readyState === 4) {
                 imgCount--;
                 if(e.status === 200)
-                $('.fotorama').append("<img src=\""+ window.URL.createObjectURL(e.response) + "\">");
+                $('.fotorama').append("<img src=\""+ window.URL.createObjectURL(e.response) + "\"" + "data-caption=\""+ link.title+ "\">");
             }
             if(imgCount === 0) {
                 $('.fotorama').fotorama();
@@ -67,14 +66,20 @@ function processLinks(links) {
             }
         
         }
-        xhr.open('GET', link);
+        xhr.open('GET', link.url);
         xhr.responseType = 'blob';
         xhr.send();
   });
 }
 
+function getRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
 function refresh() {
-    getURLs('aww', processLinks);
+    subreddits = ['aww','earthporn', 'foodporn', 'roomporn', 'urbanexploration'];
+    indx = getRandomInRange(0,subreddits.length - 1) 
+    getURLs(subreddits[indx], processLinks);
 }
 
 setInterval(refresh, 24*60*60*1000);
